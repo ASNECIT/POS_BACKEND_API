4fe152ebd41c86cc3430e70d66c43268c5d0c275
DevProsper <prosperngouari38@gmail.com>
2024-02-09 23:43:36 +0100

    Mise en place de l'authentification par JWT

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,10 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Exception;
 use Throwable;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
@@ -25,6 +27,25 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function(Exception $e, Request $request){
+            if($request->is('api/*')){
+                $data['exception_message'] = $e->getMessage();
+                $data['exception_code'] = $e->getCode();
+                $data['exception_line'] = $e->getLine();
+                $data['exception_file'] = $e->getFile();
+                \Log::error("Exception message" .$data['exception_message'].
+                " __LINE__ " .$data["exception_line"]. " __Code__ " .$data["exception_code"]. " __file__ "
+                .$data["exception_file"]);
+
+                return response()->json(
+                    [
+                        "error" => true, 
+                        "message" => $data["exception_message"],
+                        "data" => $data
+                ], 403);
+            }
         });
     }
 }
